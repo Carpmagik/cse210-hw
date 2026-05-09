cd4e671938cf9c9d825ea65f6939e2b345ea39db
Carpmagik <daviswhill9@gmail.com>
2026-05-09 19:01:53 -0400

    scripture

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,14 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        Fraction(wholeNumber: int)
+
+
+    }
+
+}
+        
---
+++ prove/Develop03/Scriputre.cs
@@ -0,0 +1,59 @@
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide && !IsCompletelyHidden())
+        {
+            int randomIndex = _random.Next(_words.Count);
+
+            if (!_words[randomIndex].IsHidden())
+            {
+                _words[randomIndex].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+
+        return displayText;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
