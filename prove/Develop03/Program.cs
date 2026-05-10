254d4e254ca2ff9a457a493844d18472d99d0ba3
Carpmagik <daviswhill9@gmail.com>
2026-05-09 20:17:01 -0400

    scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,10 +7,8 @@ class Program
     {
         Reference reference = new Reference("Proverbs", 3, 5, 6);
 
-        Scripture scripture = new Scripture(
-            reference,
-            "Trust in the Lord with all thine heart and lean not unto thine own understanding. In all thy ways acknowledge him and he shall direct thy paths."
-        );
+        Scripture scripture = new Scripture(reference,
+            "Trust in the Lord with all thine heart and lean not unto thine own understanding. In all thy ways acknowledge him and he shall direct thy paths.");
 
         while (!scripture.IsCompletelyHidden())
         {
@@ -34,6 +32,6 @@ class Program
         Console.Clear();
         Console.WriteLine(scripture.GetDisplayText());
         Console.WriteLine();
-        Console.WriteLine("Program ended");
+        Console.WriteLine("Program has ended");
     }
 }
